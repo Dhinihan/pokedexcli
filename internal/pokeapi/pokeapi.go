02729ad720f89a9d64b0f2bafed6e38907981c71
Vinícius Nascimento Silva <vinicius.nas.silva@gmail.com>
2026-02-08 14:29:51 -0300

    Cria primeira versão de pokeapi com getLocation

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,5 +1,78 @@
 package pokeapi
 
-func Ping() string {
-	return "pong"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const realUrl = "https://pokeapi.co/api/v2/"
+
+type Location struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type pokeapi struct {
+	client  *http.Client
+	baseUrl string
+}
+
+func NewPokeapi() *pokeapi {
+	return &pokeapi{
+		client:  http.DefaultClient,
+		baseUrl: realUrl,
+	}
+}
+
+type queryParams map[string]string
+
+func (p *pokeapi) GetLocation(limit int, offset int) ([]Location, error) {
+	params := queryParams{
+		"limit":  fmt.Sprintf("%d", limit),
+		"offset": fmt.Sprintf("%d", offset),
+	}
+	var locations []Location
+	err := getRequest(p, "location-area", params, &locations)
+	if err != nil {
+		return []Location{}, fmt.Errorf("Error getting location: %w", err)
+	}
+
+	return locations, nil
+}
+
+func getRequest[T any](p *pokeapi, path string, qp queryParams, dataContainer *[]T) error {
+	fullUrl := p.baseUrl + "/" + path + qp.String()
+
+	request, err := http.NewRequest(http.MethodGet, fullUrl, nil)
+	if err != nil {
+		return fmt.Errorf("Erro ao montar request: %w", err)
+	}
+	res, err := p.client.Do(request)
+
+	if err != nil {
+		return fmt.Errorf("Erro ao chamar GET %s: %w", fullUrl, err)
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("Erro ao processar GET %s: %w", fullUrl, err)
+	}
+	if err := json.Unmarshal(data, dataContainer); err != nil {
+		return fmt.Errorf("Erro ao decodificar json %s: %w", data, err)
+	}
+	return nil
+}
+
+func (qp queryParams) String() string {
+	if len(qp) == 0 {
+		return ""
+	}
+	pairs := []string{}
+	for chave, valor := range qp {
+		pairs = append(pairs, chave+"="+valor)
+	}
+	return "?" + strings.Join(pairs, "&")
 }
---
+++ internal/pokeapi/pokeapi_test.go
@@ -0,0 +1,107 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func locationsToBytes(locs []Location) ([]byte, error) {
+	data, err := json.Marshal(locs)
+	if err != nil {
+		return []byte{}, fmt.Errorf("Error parsing locations to json: %w", err)
+	}
+	return data, nil
+}
+
+func TestGetLocations(t *testing.T) {
+	loc1 := Location{
+		Name: "Teste",
+		URL:  "Teste",
+	}
+	loc2 := Location{
+		Name: "Teste2",
+		URL:  "Teste",
+	}
+	type testCase struct {
+		limit  int
+		offset int
+		expLoc []Location
+	}
+	testCases := []testCase{
+		{20, 1, []Location{loc2}},
+		{20, 0, []Location{loc1, loc2}},
+		{20, 2, []Location{}},
+		{01, 0, []Location{loc1}},
+	}
+
+	for index, tc := range testCases {
+		name := "limit: %d Offset: %d"
+		t.Run(fmt.Sprintf(name, tc.limit, tc.offset), func(tt *testing.T) {
+			tt.Parallel()
+			hf := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != "/location-area" {
+					tt.Errorf(
+						"test %d, Chamou %s ao invés de /location-area",
+						index,
+						r.URL.Path,
+					)
+				}
+				if r.URL.Query().Get("limit") != fmt.Sprintf("%d", tc.limit) {
+					tt.Errorf(
+						"Expected limit=%d, received %s",
+						tc.limit,
+						r.URL.Query().Get("limit"),
+					)
+				}
+				if r.URL.Query().Get("offset") != fmt.Sprintf("%d", tc.offset) {
+					tt.Errorf(
+						"Expected offset=%d, received %s",
+						tc.offset,
+						r.URL.Query().Get("offset"),
+					)
+				}
+				data, err := locationsToBytes(tc.expLoc)
+				if err != nil {
+					fmt.Printf("Warning: %s", err.Error())
+				}
+				_, err = w.Write(data)
+				if err != nil {
+					fmt.Printf("Warning: %s", err.Error())
+				}
+			})
+			ts := httptest.NewServer(hf)
+			tt.Cleanup(func() {
+				ts.Close()
+			})
+
+			pa := &pokeapi{
+				ts.Client(),
+				ts.URL,
+			}
+
+			locs, err := pa.GetLocation(tc.limit, tc.offset)
+			if err != nil {
+				tt.Errorf("UnexpectedError: %s", err.Error())
+			}
+
+			if len(locs) != len(tc.expLoc) {
+				tt.Errorf(
+					"test %d, received %d locations, expected %d",
+					index,
+					len(locs),
+					len(tc.expLoc),
+				)
+			}
+			for i, l := range locs {
+				if diff := cmp.Diff(l, tc.expLoc[i]); diff != "" {
+					tt.Errorf("test %d: mismatch locations:\n%s", index, diff)
+				}
+			}
+		})
+	}
+}
